43610750ec215d3176e5de0bd9001c3ba9fae268
entieerramesofia <aperezjo@centro.edu.mx>
2026-04-14 12:03:57 -0600

    colorjs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-document.addEventListener("click", function() {
+Let red = 0, green = 0, blue = 0;
+
+
+document.addEventListener("click", changeColor() {
 
 
     document.body.style.backgroundColor = "red";
 })
+
+function changeColor() {
+const rgb = "rgb("+ red + "," + green + "," + blue + ")";
+document.body.style.backgroundColor = rgb;
+}
