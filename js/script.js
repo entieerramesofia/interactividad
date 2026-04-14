a34aec13f48c2e19cd1bf61551e6881dfe1fdcb6
entieerramesofia <aperezjo@centro.edu.mx>
2026-04-14 12:40:05 -0600

    colorpantallajs

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
+/*
 Let red = 0, green = 0, blue = 0;
 
 
-document.addEventListener("click", changeColor() {
+document.addEventListener("click", changeColor)
 
-
-    document.body.style.backgroundColor = "red";
-})
 
 function changeColor() {
-const rgb = "rgb("+ red + "," + green + "," + blue + ")";
-document.body.style.backgroundColor = rgb;
+
+    red = Math.random() * 255;
+    green = Math.random() * 255;
+    blue = Math.random() * 255;
+
+
+    const rgb = "rgb(" + red + "," + green + "," + blue + ")";
+    document.body.style.backgroundColor = rgb;
+}
+
+*/
+
+
+
+
+document.addEventListener("click", changeColor);
+
+function changeColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const rgb = `rgb(${red}, ${green}, ${blue})`;
+    document.body.style.backgroundColor = rgb;
 }
