b7e87169e58c07318772445f78e201aad153b21e
entieerramesofia <aperezjo@centro.edu.mx>
2026-04-21 20:01:01 -0600

    p5color

--- js/p5.js
+++ js/p5.js
@@ -1,7 +1,20 @@
+let size = 120;
+
 function setup() {
-createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(200,55,0);
+  background(200, 55, 0, 50);
+
+  // Mapear posición del mouse a colores
+  let r = map(mouseX, 0, width, 0, 255);
+  let g = map(mouseY, 0, height, 0, 255);
+  let b = map(mouseX + mouseY, 0, width + height, 0, 255);
+
+  fill(r, g, b);
+  noStroke();
+
+  rect(mouseX, mouseY, size, size);
 }
